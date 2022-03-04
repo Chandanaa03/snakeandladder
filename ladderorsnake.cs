e43bf2d7557ca63fc96165a94341baaadf45912a
chandana03 <chandanareshma01@gmail.com>
2022-03-05 01:13:25 +0530

    ladder or snake

---
+++ ladderorsnake.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeandladder
+{
+    public class ladderorsnake
+    {
+        public static void opition()
+        {
+            int player1, playerposition;
+            playerposition = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine(" number on dice :" + dice);
+
+            
+
+               
+
+
+             
+                switch (dice)
+                {
+
+                    case 1:
+                        dice = 1;
+                        Console.WriteLine("The player1 has no play");
+                        break;
+                    case 2:
+                        dice = 2;
+                        player1 = playerposition - dice;
+                        Console.WriteLine("Oops it snake");
+                        break;
+                    case 3:
+                        dice = 3;
+                        Console.WriteLine("The player1 has no play");
+                        break;
+                    case 4:
+                        dice = 4;
+                        player1 = playerposition + dice;
+                        Console.WriteLine("Hurray! Its ladder time");
+                        break;
+                    case 5:
+                        dice = 5;
+                        player1 = playerposition - dice;
+                        Console.WriteLine("Oops it snake");
+                        break;
+                    case 6:
+                        dice = 6;
+                        player1 = playerposition + dice;
+                        Console.WriteLine("Hurray! Its ladder time");
+                        break;
+                }
+
+            }
+        }
+    }
+
+
+
+
+        
