2999cf7da8e48ea7aa0cef998658a4fbc78265e4
chandana03 <chandanareshma01@gmail.com>
2022-03-05 00:56:15 +0530

    snake and ladder

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace snakeandladder
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //startposition.start_position();
+
+            //randomnumber.dice();
+
+            // ladderorsnake.opition();
+
+            playerposition.position();
+        }
+    }
+}
