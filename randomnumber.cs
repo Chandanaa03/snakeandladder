aacfa594945a11f6c48f99a6f9f9616efaeeb1e9
chandana03 <chandanareshma01@gmail.com>
2022-03-05 01:02:37 +0530

    random number of dice

---
+++ randomnumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeandladder
+{
+    public class randomnumber
+    {
+        public static void dice()
+        {
+            int player1, playerposition;
+
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("player1 dice value is:" + die);
+        }
+    }
+}
