113bb1db07048d05e9c89120353174813426bc01
chandana03 <chandanareshma01@gmail.com>
2022-03-05 00:59:10 +0530

    start position of player

---
+++ startposition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeandladder
+{
+    public class startposition
+    {
+        public static void start_position()
+        {
+            int player1, playerposition;
+
+            playerposition = 0;
+
+            Console.WriteLine("The player1 position is:" + playerposition);
+
+
+
+           
+        }
+    }
+}
